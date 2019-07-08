337933b8b287544da7b4164bbdca42c43fd65346
Tony <tony.n.flpa@gmail.com>
2019-07-08 10:03:56 -0400

    added mysql

--- API.js
+++ API.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const mysql      = require('mysql');
 
-const app = express();
 
 
-// listening
-app.listen(5000, function(err){
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'admin',
+  password : 'student'
+});
+
+db.connect(function(err){
+    if (err) throw err
+    console.log("MySQL is connected, lookin shakin bacon!")
+});
+
+const api = express();
+
+api.listen(5000, function(err){
     if (err)
         console.log(err)
     console.log('Server is live on port 5000')
